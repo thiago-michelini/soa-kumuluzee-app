6a7e219d18bb76b1daba9641735e205bcced6838
thiago.michelini <thiago.michelini@redetendencia.com.br>
2016-07-13 15:08:36 -0400

    #commit máquina do trabalho

---
+++ src/main/java/org/kumuluzee/servlets/HelloWorldController.java
@@ -0,0 +1,23 @@
+package org.kumuluzee.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello-world")
+public class HelloWorldController extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/plain");
+		resp.setStatus(HttpServletResponse.SC_OK);
+		resp.getWriter().println("Hello World KumuluzEE");
+	}
+	
+}
